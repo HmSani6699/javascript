ebb24d09c659b21ba80b586a6cd8402b63a2dbde
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-01-26 13:18:17 +0600

    add to oad number

---
+++ variavle_name.js
@@ -0,0 +1,39 @@
+
+//snake case
+var tomar_name = "sadikur rahman sani";
+// console.log(tomar_name);
+
+var tomar_bari = "pakunda";
+// console.log(tomar_bari);
+
+//camel case 
+var pakundaMsdrasa = "sontga natayoung";
+// console.log(pakundaMsdrasa);
+
+
+//pascal case
+var PascalCase = 'hallo ami pascal case boltasi';
+// console.log(PascalCase);
+
+var number1 = 50;
+var number2 = 20;
+// var sum = number1 + number2;
+// var total = sum + 10;
+
+var debit = number1 - number2;
+
+
+var apple = 10;
+var lagbo = 5;
+
+var gon = apple * lagbo;
+
+
+var tak = 100;
+var man = 10;
+var allman = tak / man;
+
+var nam = "adiba";
+// console.log(nam);
+nam = "rima";
+console.log(nam)
