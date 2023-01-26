5ae2e7176984ea1c3cd04e59ada6216954a55925
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-01-26 18:42:42 +0600

    add to variavle name declar

---
+++ 17__1-9/variable.js
@@ -0,0 +1,13 @@
+//Numeric
+var age = 39;
+var dam = 400;
+var adnan = 500;
+
+//string
+var name = "hm sadikur rahman sani";
+var address = "pakunda";
+
+
+//boolean
+var tomiVallo = true;
+var amiVallo = false;
---
+++ 17__1-9/variavle_name.js
@@ -0,0 +1,85 @@
+
+//snake case
+var tomar_name = "sadikur rahman sani";
+// console.log(tomar_name);
+
+var tomar_bari = "pakunda";
+// console.log(tomar_bari);
+
+//camel case 
+var pakundaMsdrasa = "sontga natayoung";
+// console.log(pakundaMsdrasa);
+
+
+//pascal case
+var PascalCase = 'hallo ami pascal case boltasi';
+// console.log(PascalCase);
+
+var number1 = 50;
+var number2 = 20;
+// var sum = number1 + number2;
+// var total = sum + 10;
+
+var debit = number1 - number2;
+
+
+var apple = 10;
+var lagbo = 5;
+
+var gon = apple * lagbo;
+
+
+var tak = 100;
+var man = 10;
+var allman = tak / man;
+
+var nam = "adiba";
+// console.log(nam);
+nam = "rima";
+// console.log(nam);
+
+
+var num1 = 10;
+num1 = num1 + 20;
+num1 += 10;
+num1++;
+
+
+var firstName = "Md";
+var lastName = "Sadiq";
+var fullName = firstName + " " + lastName;
+
+var number = '22';
+var number1 = 10;
+var total = parseInt(number) + number1;
+
+
+
+var num = 0.1;
+var num1 = 0.2;
+var total = num1 + num;
+total = parseFloat(total.toFixed(4));
+// total = parseFloat(total);
+
+// console.log(total);
+
+
+//snake case
+var tomar_name_ki = "Sani";
+var tomar_bari = "pakunda";
+var tomar_location = "sonarga";
+
+
+//camel case
+var tomarNameki = "Sani";
+var tomarBariKoi = "Sonarga";
+var tomarLocationKoi = "pakunda";
+
+
+//pascal case
+var TomarNameKi = "Hm Sadiq";
+var TomarBariKoi = "Narayoun gomj";
+var TomarlocarionKoi = "Pakunda";
+
+
+
