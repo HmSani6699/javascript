7471403d81b18042a3e328cbee5fffe4dd1b5d1d
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-01-26 19:10:37 +0600

    ad to shorthand section

---
+++ 17__1-9/shorthand.js
@@ -0,0 +1,16 @@
+//chance your variable name
+var name = "Hm Sani";
+name = "Hm Sadiqur Rahman Sani";
+// console.log(name);
+
+
+//
+var number = 20;
+number = number + 10;
+
+//shorthand item 1
+number += 10;
+
+//shorthand item 2
+number++;
+console.log(number);
