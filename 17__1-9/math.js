e985e5a96ed2a901dd2db9b391c96c82ab761be1
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-01-26 19:01:03 +0600

    add to math part

---
+++ 17__1-9/math.js
@@ -0,0 +1,34 @@
+//sum
+var alo = 30;
+var roson = 40;
+var tomato = 30;
+var total = alo + roson + tomato;
+// console.log(total);
+
+
+//bat dawoya
+var tk = 1000;
+var anika = 300;
+var totalTk = tk - anika;
+// console.log(totalTk);
+
+//multi plecation
+var panPrice = 10;
+var panLgbo = 5;
+var allTak = panPrice * panLgbo;
+// console.log(allTak);
+
+
+//debaging 
+var allAmound = 500;
+var friends = 15;
+var parPic = allAmound / friends;
+// console.log(parPic);
+
+
+//bag foll
+
+var man = 20;
+var te = 12;
+var bagFall = man % te;
+console.log(bagFall);
