f1da18c649a82f5bf61b903cfffd3816f3058b0f
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-04 23:51:05 +0600

    ad to variable part

---
+++ js-basic-problem-23/variable.js
@@ -0,0 +1,14 @@
+var name = 'sani';
+var age = 19;
+var valo = true;
+
+
+/* ---let or const */
+
+let name_1 = 'numan';
+name_1 = 'monna'
+
+
+const number = 30;
+number = 40 //node allow
+console.log(number)
