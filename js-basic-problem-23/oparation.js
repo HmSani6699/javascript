a3c44e8bcc186299b42f9e3270ca4761cfe6a14c
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-04 23:59:21 +0600

    ad to math oparation part

---
+++ js-basic-problem-23/oparation.js
@@ -0,0 +1,37 @@
+/* -----++++++++++++------- */
+let num1 = 20;
+let num2 = 10;
+let total = num1 + num2;
+// console.log(total)
+
+/* ------------------- */
+
+var num_1 = 10;
+var num_2 = 5;
+let mines = num_1 - num_2;
+// console.log(mines)
+
+
+
+/* --------***********------- */
+
+var num_1 = 10;
+var num_2 = 5;
+let multiplay = num_1 * num_2;
+// console.log(multiplay)
+
+
+/* ------/////////-------- */
+
+var num_1 = 10;
+var num_2 = 5;
+let dibaging = num_1 / num_2;
+// console.log(dibaging)
+
+
+/* -------% % % % % % ------ */
+
+var num_1 = 10;
+var num_2 = 5;
+let modulas = num_1 % num_2;
+console.log(modulas)
