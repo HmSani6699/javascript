5d1e018a5df86f36106de60fa1e10988acaec26a
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-05 01:48:26 +0600

    ad to problem 5

---
+++ js-basic-problem-23/problem-5.js
@@ -0,0 +1,10 @@
+let numbers = [10, 40, 28, 47, -50, 39, 20, 58];
+
+let newArray = []
+for (number of numbers) {
+    if (number < 0) {
+        break;
+    }
+    newArray.push(number)
+}
+console.log(newArray)
