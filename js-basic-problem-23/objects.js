027a2248c916027e785ed49188dabe2e8b9b0a29
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-05 00:21:20 +0600

    ad to onjects part

---
+++ js-basic-problem-23/objects.js
@@ -0,0 +1,13 @@
+/* 
+১২. একটা অবজেক্ট ডিক্লেয়ার করবে। সেটাতে তিনটা প্রপার্টি থাকবে। এবং কোন একটা প্রোপার্টি এর মান চেইঞ্জ করবা। 
+*/
+
+let ami = {
+    name: 'sani',
+    age: 19,
+    address: 'pakunda'
+}
+
+//change value
+ami.address = 'paunda nama para'
+console.log(ami)
