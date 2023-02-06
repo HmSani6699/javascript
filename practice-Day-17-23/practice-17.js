0e2f124106928eb3c21465f78fbc13af9ed468c0
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-06 18:58:17 +0600

    ad to javascript data type part

---
+++ practice-Day-17-23/practice-17.js
@@ -0,0 +1,39 @@
+/* ----Declare tha variable */
+/*
+ var name = 'Sadiqur Rahman Sani';
+// console.log(name)
+
+*/
+
+//-------------------------------------------------------------//
+
+
+/* -------Variable Data type --------*/
+/*
+ // 1. Numeric
+let num1 = 30;
+let num2 = 20;
+
+// 2. String
+let name_1 = 'Asiba';
+let friends = 'Thalha';
+
+// 3. Boolean
+let amiVallo = true;
+let amiKharab = false;
+
+// 4. Null
+let madrasa = null;
+// console.log(madrasa)
+
+// 5.Undefined
+let pan;
+console.log(pan);
+
+// 6. Symbol
+
+// 7.Objects
+ */
+
+
+
--- practice-Day-17-23/problem-2.js
+++ practice-Day-17-23/problem-2.js
@@ -11,4 +11,87 @@ arr.sort(function (a, b) {
     return a - b;
 });
 
-console.log(arr[arr.length - 2], arr);
+// console.log(arr[arr.length - 2], arr);
+
+
+/* let factorial = 1;
+for (let i = 1; i <= 10; i++) {
+    factorial *= i 
+}
+// console.log(factorial)
+*/
+
+
+function factorial(n) {
+    let answer = 1;
+    if (n == 0 || n == 1) {
+        return answer;
+    }
+    else if (n > 1) {
+        for (var i = n; i >= 1; i--) {
+            answer = answer * i;
+        }
+        return answer;
+    }
+    else {
+        return "number has to be positive."
+    }
+}
+let n = 1;
+// answer = factorial(n)
+// console.log("Factorial of " + n + " : " + answer);
+
+
+
+function average(arrOfMarks) {
+    //write your code here
+    // don't forget to write return
+    let sum = 0;
+    for (let i = 0; i < arrOfMarks.length; i++) {
+        let element = arrOfMarks[i];
+        sum += element
+    }
+    let gorNumber = sum / arrOfMarks.length;
+    let number = gorNumber.toFixed(2);
+    if (typeof number === 'number') {
+        return number
+    }
+
+}
+
+
+function checkTriangle(side1, side2, side3) {
+    let num = '';
+    if (side1 > side2 || side1 > side3) {
+        num = 'Yes'
+    }
+    else {
+        num = 'No'
+    }
+    return num
+}
+
+console.log(checkTriangle(6, 8, 6))
+
+
+
+
+
+
+
+
+
+
+/* Input:
+
+Output:
+"YES"
+﻿
+Case 2:
+Input:
+[
+    {
+        lastName: 'Mithila',
+    }, 'fname'
+]
+Output: */
