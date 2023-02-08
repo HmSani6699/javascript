e27f2832e8f11eeec025480b453574e9f0b0e8b2
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-08 17:37:08 +0600

    ad to nestad array part

---
+++ bonus-video-24/nested-array.js
@@ -0,0 +1,14 @@
+let subject = ['Bangla', [
+    {
+        name: 'asik',
+        age: 30,
+        location: 'bogora'
+    }
+],
+    ['bangla bakoron', 'bangla sahittik', [23, 45, 67, 2]],
+
+    'arbi', ['ordo', 'arbi', 'kayda']];
+
+let a = subject[1][0].name
+console.log(a)
+
