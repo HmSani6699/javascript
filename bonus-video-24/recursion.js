0e81d36fa1a306590114b4cea6489c0568c2b444
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-08 14:38:03 +0600

    ad to function recurson part

---
+++ bonus-video-24/recursion.js
@@ -0,0 +1,22 @@
+let sum_1 = 0;
+for (let i = 0; i <= 5; i++) {
+    sum_1 += i
+}
+// console.log(sum_1)
+
+
+function sum(num) {
+    if (num == 1) {
+        return num
+    }
+    return num + sum(num - 1);
+}
+
+/* 
+5 + sum(4)
+5 + 4 + sum(3)
+5 + 4 + 3 + sum(2)
+5 + 4 + 3 + 2 + sum(1)
+5 + 4 + 3 + 2 + 1 sum(0)
+5 + 4 + 3 + 2 + 1 + 0
+*/
