dee987cc7337b878bda652a27184c705e2312291
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-09 00:22:34 +0600

    ad dom part

---
+++ jvascript-DOM/25_1-9/js/index.js
@@ -0,0 +1,15 @@
+//getElementById
+let listItem = document.getElementById('list-item');
+console.log(listItem)
+
+//getElementsByClassName
+let items = document.getElementsByClassName('list-item');
+console.log(items);
+
+//querySelector
+let bookList = document.querySelector('#book-list');
+console.log(bookList)
+
+//querySelectorAll
+let panList = document.querySelectorAll('pan-list');
+console.log(panList)
