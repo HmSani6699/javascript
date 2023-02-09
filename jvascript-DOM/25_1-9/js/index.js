60d7b113ebfe4be0f262e136a3a4c74eb5066f5c
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-09 07:52:08 +0600

    ad to innerText and innerHtml

--- jvascript-DOM/25_1-9/js/index.js
+++ jvascript-DOM/25_1-9/js/index.js
@@ -1,10 +1,13 @@
 //getElementByTagName
 let tagNames = document.getElementsByTagName('h1');
+
 for (const tagName of tagNames) {
     let background = tagName.style.background = "yellow";
     let border = tagName.style.border = ' 2px solid red';
     let padding = tagName.style.padding = ' 20px';
-    console.log(padding)
+    let textCenter = tagName.style.textAlign = ' center';
+    let borderTopRightRadius = tagName.style.borderTopRightRadius = '20px';
+    let borderRadius = tagName.style.borderBottomLeftRadius = '20px';
 }
 
 //getElementById
@@ -22,4 +25,27 @@ let bookList = document.querySelector('#book-list');
 //querySelectorAll
 let panList = document.querySelectorAll('pan-list');
 // console.log(panList)
+
+
+//getAttribute
+let container = document.getElementsByClassName('border');
+// console.log(container[0].getAttribute('title'))
+
+//classList
+// console.log(container[0].classList.add('tttttt'));
+// console.log(container[0].classList.remove('tttttt'));
+// console.log(container[0].classList)
+
+
+// setAttribute
+// let btn = document.getElementById('input');
+// console.log(btn.setAttribute('type', 'button'))
+// console.log(btn.setAttribute('value', 'button'))
+
+//innerText
+let getText = document.getElementById('bang');
+// console.log(getText.innerText)
+
+//innerHtml
+// console.log(getText.innerHTML)
 
