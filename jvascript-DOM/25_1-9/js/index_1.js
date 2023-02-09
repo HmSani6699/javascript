3d681344b45cf59f03b8d92608e7d4243a61fcd2
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-09 12:02:49 +0600

    javascript attribut part

--- jvascript-DOM/25_1-9/js/index_1.js
+++ jvascript-DOM/25_1-9/js/index_1.js
@@ -1,4 +1,4 @@
-console.log(4534535);
+// console.log(4534535);
 
 //1.getElementByTagName
 let bookContainer = document.getElementsByTagName('li');
@@ -13,9 +13,37 @@ let title = document.getElementsByClassName('title');
 // console.log(title)
 
 //querySelectorAll
-let li = document.querySelectorAll('li');
-// console.log(li)
+let li_s = document.querySelectorAll('li');
+// console.log(li_s)
 
 //querySelector
 let arbi = document.querySelector('.arbi');
 // console.log(arbi)
+
+
+/* -------------check attribute---------*/
+
+//getAttribute
+let getAttribute = title[0].getAttribute('class')
+// console.log(getAttribute);
+
+//setAttribute
+for (let li of li_s) {
+    li.setAttribute('class', 'li_color');
+};
+
+//classList
+let checkClassList = document.getElementById('second-title').classList;
+for (let key of checkClassList.keys()) {
+    // console.log(key)
+}
+
+//classList.add()
+let thirdTitle = document.getElementById('third-title');
+let addClassList = thirdTitle.classList.add('third');
+console.log(thirdTitle)
+
+//classList.remove
+let removeClassList = thirdTitle.classList.remove('third');
+
+
