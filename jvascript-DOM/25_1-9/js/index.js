8ac7cc1d86da9c986dcbedd5a2fbfd726d5ec684
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-02-09 06:57:41 +0600

    ad javascript style part

--- jvascript-DOM/25_1-9/js/index.js
+++ jvascript-DOM/25_1-9/js/index.js
@@ -1,15 +1,25 @@
+//getElementByTagName
+let tagNames = document.getElementsByTagName('h1');
+for (const tagName of tagNames) {
+    let background = tagName.style.background = "yellow";
+    let border = tagName.style.border = ' 2px solid red';
+    let padding = tagName.style.padding = ' 20px';
+    console.log(padding)
+}
+
 //getElementById
 let listItem = document.getElementById('list-item');
-console.log(listItem)
+// console.log(listItem)
 
 //getElementsByClassName
 let items = document.getElementsByClassName('list-item');
-console.log(items);
+// console.log(items);
 
 //querySelector
 let bookList = document.querySelector('#book-list');
-console.log(bookList)
+// console.log(bookList)
 
 //querySelectorAll
 let panList = document.querySelectorAll('pan-list');
-console.log(panList)
+// console.log(panList)
+
